2016798546f74570823b55db65996113a7e7d89c
Vadim L. Avramishin <vavramishin@brightgrove.com>
2017-07-24 20:21:34 +0300

    add orders flow

--- app/controllers/ikajo/sale/create.php
+++ app/controllers/ikajo/sale/create.php
@@ -102,7 +102,7 @@ try {
     curl_setopt($ch, CURLOPT_HEADER, false);
     curl_setopt($ch, CURLOPT_POST, true);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
     curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
     $data = curl_exec($ch);
