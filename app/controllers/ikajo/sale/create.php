d8dcc052f3d0cf334d4d2e04838d78ad2271f950
Vadim L. Avramishin <vavramishin@brightgrove.com>
2017-08-30 22:33:08 +0300

    set CURL_SSLVERSION_TLSv1_2

--- app/controllers/ikajo/sale/create.php
+++ app/controllers/ikajo/sale/create.php
@@ -115,7 +115,7 @@ try {
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
-    curl_setopt($ch, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
+    curl_setopt($ch, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1 );
 
     curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
     $data = curl_exec($ch);
